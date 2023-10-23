2044cf04cc2403912cc4678993e79d1d0487ea7c
W4NTER <snegovikvadim@gmail.com>
2023-10-23 21:13:32 +0500

    changes to run checkstyle

--- src/main/java/edu/project1/ConsoleHangman.java
+++ src/main/java/edu/project1/ConsoleHangman.java
@@ -1,9 +1,9 @@
 package edu.project1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import java.util.Arrays;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class ConsoleHangman {
